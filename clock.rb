e8d49718e92bdef876ed5f08941203831bf2620f
yswallow <y.sanagi+prog@auone.jp>
2012-09-11 10:33:43 +0900

    インデントの修正，例外処理の追加

--- clock.rb
+++ clock.rb
@@ -41,68 +41,71 @@ handler do |job|
 	
 	gmail = Gmail.new(USERNAME,PASSWORD)
 	
-	mails = gmail.inbox.emails(:unread,:from => FROM).each do |mail|
+	begin
+		mails = gmail.inbox.emails(:unread,:from => FROM).each do |mail|
+		
+			subject = mail.subject.toutf8
+			from = full_address(mail.from)
+			to = full_address(mail.to)
+		
+			puts "From: #{from}"
+			puts "Subject: #{subject}"
+			puts "To: #{to}"
+			
+			body = String.new
+			if mail.text_part
+				puts "text: #{mail.text_part.decoded}"
+				body = mail.text_part.decoded
+			elsif mail.html_part
+				puts "html: #{mail.html_part.decoded}"
+				body = mail.html_part.decoded
+			elsif
+				puts "body: #{mail.body.decoded.encode("UTF-8",mail.charset)}"
+				body = mail.body.decoded.encode("UTF-8",mail.charset)
+			end
+		
+			puts "body:"
+			p body
+			title = subject
+			next unless to == USERNAME
+			
+			page_title = subject.chomp
+			title_length = title.length
+			i = 0
+			str = String.new
+			long_url = String.new
+			p body
+			body.each_line do |line|
+				if /^http:\/\// =~ line
+					puts "URL: #{line}"
+					long_url = line.chomp
+#					long_url[i] = line.chomp 
+#					i += 1
+				elsif line.include?("@")
+					next
+				elsif line.include?("Sent from Pocket - Get it free!")
+					next
+				else
+					str << line
+				end
+			end
+		
+			str.chomp!
+			short_url = url_short(long_url)["url"]
+			tweet_str = "#{str} \n/ #{title} \n/ #{short_url}"
+		 
+			Twitter.update(tweet_str)
+		
+			puts "ツイートしました！"
+			puts tweet_str
+			print "\n\n"
 	
-	subject = mail.subject.toutf8
-	from = full_address(mail.from)
-	to = full_address(mail.to)
-	
-	puts "From: #{from}"
-	puts "Subject: #{subject}"
-	puts "To: #{to}"
-	
-	body = String.new
-	if mail.text_part
-		puts "text: #{mail.text_part.decoded}"
-		body = mail.text_part.decoded
-	elsif mail.html_part
-		puts "html: #{mail.html_part.decoded}"
-		body = mail.html_part.decoded
-	elsif
-		puts "body: #{mail.body.decoded.encode("UTF-8",mail.charset)}"
-		body = mail.body.decoded.encode("UTF-8",mail.charset)
-	end
-	
-	puts "body:"
-	p body
-	title = subject
-	next unless to == USERNAME
-	
-	page_title = subject.chomp
-	title_length = title.length
-	i = 0
-	str = String.new
-	long_url = String.new
-	p body
-	body.each_line do |line|
-		if /^http:\/\// =~ line
-			puts "URL: #{line}"
-			long_url = line.chomp
-#			long_url[i] = line.chomp 
-#			i += 1
-		elsif line.include?("@")
-			next
-		elsif line.include?("Sent from Pocket - Get it free!")
-			next
-		else
-			str << line
 		end
+	rescue => ex
+		puts ex.message
+	ensure
+		gmail.logout
 	end
-	
-	str.chomp!
-	short_url = url_short(long_url)["url"]
-	tweet_str = "#{str} \n/ #{title} \n/ #{short_url}"
-	 
-	Twitter.update(tweet_str)
-	
-	puts "ツイートしました！"
-	puts tweet_str
-	print "\n\n"
-
-end
-
-gmail.logout
-
 end
 
 every(10.seconds, 'mail_to_tweet.job')
